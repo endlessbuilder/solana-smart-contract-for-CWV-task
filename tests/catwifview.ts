29491d671c71c69f326856e4aa761707b1d6e616
endlessbuilder <continuebuilding@gmail.com>
2024-09-01 09:44:07 +0200

    add: contants

--- tests/catwifview.ts
+++ tests/catwifview.ts
@@ -1,16 +1,25 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Catwifview } from "../target/types/catwifview";
+import * as anchor from '@coral-xyz/anchor'
+import { Program } from '@coral-xyz/anchor'
+import { Catwifview } from '../target/types/catwifview'
+import { Keypair } from '@solana/web3.js'
 
-describe("catwifview", () => {
+describe('catwifview', () => {
+  const adminKeypair = Keypair.generate()
+  const userKeyparit = Keypair.generate()
+
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(anchor.AnchorProvider.env())
 
-  const program = anchor.workspace.Catwifview as Program<Catwifview>;
+  const program = anchor.workspace.Catwifview as Program<Catwifview>
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+  // const connection = new Connection(ANKR_MAINNET, {
+  //   wsEndpoint: ANKR_MAINNET_WS,
+  //   commitment: 'finalized'
+  // })
+  // const provider = new AnchorProvider(connection, new Wallet(adminKeypair))
+  // const program = new Program<Middleman>(idl as any, provider)
+
+  it('Is initialized!', async () => {
+    
+  })
+})
